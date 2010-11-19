1e00c866c527873621cf691d45933a8c4f7d05e7
Michael Schade <michaelschade@sddomain.com>
2010-11-19 12:54:10 -0600

    Added jQuery-JSON. Network communication may not yet work, moving on to linear parser.

--- js/aus-client.js
+++ js/aus-client.js
@@ -1,2 +1,19 @@
 $(document).ready(function() {
+    function post(data) {
+        $.post("http://ht.api.accentuate.us:8080/"
+            , {js: $.toJSON(data)}
+            , function(rsp) {
+                window.alert(rsp);
+                $("#output").append(rsp.text);
+            }, "json"
+        );
+    }
+    /*
+    post({
+          "call":   "charlifter.langs"
+        , "locale": "en-US"
+        , "lang":   "ht"
+        , "text":   "Bon, la fe sa apre demen pito, le la we mwen andey."
+    });
+    */
 });
