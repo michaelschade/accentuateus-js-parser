1112f4beded7f394a38d50588ccbf16ecd666779
Michael Schade <michaelschade@sddomain.com>
2010-12-05 03:13:14 -0600

    Added comments.

--- js/aus-client.js
+++ js/aus-client.js
@@ -17,9 +17,6 @@ function Chunk(elem, oldText, newText) {
 $(document).ready(function() {
     /* Extract text from source + one context word on each side */
     function extract(chunk) {
-        /*
-        Regex: \w+\s+text
-        */
         var re      = RegExp('\\w*\\s*\\w*' + chunk.old +'\\w*\\s*\\w*', 'g');
         chunk.old   = re.exec(chunk.elem.value)[0];
         return chunk;
@@ -29,6 +26,7 @@ $(document).ready(function() {
         chunk.elem.value = chunk.elem.value.replace(chunk.old, chunk.lifted);
     }
     function post(chunk) {
+        /* Post chunk to-be-lifted text to server */
         var data = {
               call:     "charlifter.lift"
             , lang:     "ht"
@@ -40,25 +38,28 @@ $(document).ready(function() {
             , type:     "POST"
             , data:     $.toJSON(data)
             , success:  function(rsp) {
+                // Update origin with Accentuated text
                 chunk.lifted = rsp.text;
                 update(chunk);
                 $("#output").append(rsp.text + "<br />");
             } , contentType: "application/json; charset=utf-8"
         });
     }
-    var str = "";
+    var buf = "";
     process = function() {
-        if (str != "") {
-            post(extract(Chunk(document.getElementById("text"), str, '')));
-            str = "";
+        if (buf != "") {
+            post(extract(Chunk(document.getElementById("text"), buf, '')));
+            buf = "";
         }
     }
-    var re = /[.!?,]/
+    var punctuation = /[.!?,]/;
     $("input").bind("keypress", function(evt) {
+        // If not an irrelevant modifier key
         if (!(evt.altKey || evt.ctrlKey || evt.metaKey)) {
+            // Add key to buffer
             var key = String.fromCharCode(evt.which);
             str += key;
-            if (re.test(key)) {
+            if (punctuation.test(key)) { // Check for end-of-buffer signal
                 process();
             }
             clearTimeout(j);
